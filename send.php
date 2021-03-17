431997a6a3198ad5fccce6da86c358f4d8b54f81
lampa-lana <lampa1979@mail.ru>
2021-03-17 22:20:20 +0400

    create: homepage1, homepage2

---
+++ send.php
@@ -0,0 +1,63 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$email = $_POST['email'];
+$emailNews = $_POST['emailNews'];
+$password = $_POST['password'];
+$phone = $_POST['phone'];
+$text = $_POST['text'];
+
+
+// Формирование самого письма
+$title = "Новое сообщение EHYA";
+$body = "
+<h2>Новое Обращение</h2>
+<b>Имя:</b> $name<br>
+<b>Почта:</b> $email<br><br>
+<b>Пароль:</b> $password<br><br>
+<b>Подписка:</b> $emailNews<br><br>
+<b>Телефон:</b> $phone<br><br>
+<b>Сообщение:</b><br>$text
+";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты  
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'glo.svetlana.arustamyan@gmail.com'; // Логин на почте
+    $mail->Password   = 'Glo24122020'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('glo.svetlana.arustamyan@gmail.com', 'Светлана Арустамян'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('lampa-lana@yandex.ru');  
+
+    // Отправка сообщения
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body;    
+
+    // Проверяем отравленность сообщения
+    if ($mail->send()) {$result = "success";} 
+    else {$result = "error";}
+
+    } catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+    }
+
+    // Отображение результата
+    header('Location: thankyou.html');
